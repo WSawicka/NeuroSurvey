7028cb51c54481b79c3e77d189743e7cfe4248dd
WSawicka <weronika.sawicka.9@gmail.com>
2017-03-06 23:53:48 +0100

    First commit

---
+++ app/src/main/java/com/sawicka/neurosurvey/MainActivity.java
@@ -0,0 +1,20 @@
+package com.sawicka.neurosurvey;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void newTest(View view){
+        Intent intent = new Intent(this, NewTest.class);
+        startActivity(intent);
+    }
+}
