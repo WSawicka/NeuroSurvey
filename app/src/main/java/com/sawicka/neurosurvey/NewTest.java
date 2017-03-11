59e778c0a07822c267915c574701fbda75a17c9c
WSawicka <weronika.sawicka.9@gmail.com>
2017-03-11 23:40:51 +0100

    Skoki pomiędzy pytaniami - początek

--- app/src/main/java/com/sawicka/neurosurvey/NewTest.java
+++ app/src/main/java/com/sawicka/neurosurvey/NewTest.java
@@ -2,6 +2,8 @@ package com.sawicka.neurosurvey;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
 
 public class NewTest extends Activity {
 
@@ -9,5 +11,20 @@ public class NewTest extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_test);
+    }
+
+    public void onClick_1(View view){
+        View questions_2_4 = findViewById(R.id.questions_2_4);
+        View questions_13 = findViewById(R.id.questions_13);
+
+        RadioGroup group1 = (RadioGroup) findViewById(R.id.quest1_group);
+        int checkedId = group1.getCheckedRadioButtonId();
+
+        if(checkedId == R.id.quest1_ans2){
+            questions_2_4.setVisibility(View.VISIBLE);
+        } else {
+            questions_2_4.setVisibility(View.GONE);
+            questions_13.setVisibility(View.VISIBLE);
+        }
     }
 }
